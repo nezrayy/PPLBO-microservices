be69db38065c9ca2280edc32cb57be8a74ce6fdb
nezrayy <fanez.fthd@gmail.com>
2025-02-16 17:50:11 +0700

    part 4 done

--- order-service/src/main/java/com/fanez/microservices/order_service/service/OrderService.java
+++ order-service/src/main/java/com/fanez/microservices/order_service/service/OrderService.java
@@ -4,6 +4,7 @@ import java.util.UUID;
 
 import org.springframework.stereotype.Service;
 
+import com.fanez.microservices.order_service.client.InventoryClient;
 import com.fanez.microservices.order_service.dto.OrderRequest;
 import com.fanez.microservices.order_service.model.Order;
 import com.fanez.microservices.order_service.repository.OrderRepository;
@@ -15,15 +16,21 @@ import lombok.RequiredArgsConstructor;
 public class OrderService {
 
     private final OrderRepository orderRepository;
+    private final InventoryClient inventoryClient;
     public void placeOrder(OrderRequest orderRequest) {
-        //map OrderRquest to Order object
-        Order order = new Order();
-        order.setOrderNumber(UUID.randomUUID().toString());
-        order.setPrice(orderRequest.price());
-        order.setSkuCode(orderRequest.skuCode());
-        order.setQuantity(orderRequest.quantity());
-
-        //save Order object to OrderRepository
-        orderRepository.save(order);
+        var isProductInStock = inventoryClient.isInStock(orderRequest.skuCode(), orderRequest.quantity());
+        
+        if (isProductInStock) {
+            Order order = new Order();
+            order.setOrderNumber(UUID.randomUUID().toString());
+            order.setPrice(orderRequest.price());
+            order.setSkuCode(orderRequest.skuCode());
+            order.setQuantity(orderRequest.quantity());
+    
+            //save Order object to OrderRepository
+            orderRepository.save(order);
+        } else {
+            throw new RuntimeException("Product with skuCode " + orderRequest.skuCode() + " is out of stock.");
+        }
     }
 }
